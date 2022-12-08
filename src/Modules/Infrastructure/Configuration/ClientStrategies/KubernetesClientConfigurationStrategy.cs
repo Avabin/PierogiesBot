f6bd94df189b49affb898c6e81c232c092637827
Avabin <avabin@hotmail.com>
2022-12-08 19:19:25 +0100

    Add serilog

---
+++ src/Modules/Infrastructure/Configuration/ClientStrategies/KubernetesClientConfigurationStrategy.cs
@@ -0,0 +1,53 @@
+﻿using Core;
+using Orleans.Configuration;
+using Orleans.Providers.MongoDB.Configuration;
+using Orleans.Streams;
+using Streams;
+
+namespace Infrastructure.Configuration.ClientStrategies;
+
+public class KubernetesClientConfigurationStrategy : ClientConfigurationStrategyBase
+{
+    private readonly string _clusterId;
+    private readonly string _connectionString;
+    private readonly string _serviceId;
+
+    public KubernetesClientConfigurationStrategy(string clusterId, string serviceId, string? connectionString)
+    {
+        if (connectionString is null or "")
+            throw new ArgumentNullException(nameof(connectionString), "Connection string cannot be null or empty");
+        _clusterId = clusterId;
+        _serviceId = serviceId;
+        _connectionString = connectionString;
+    }
+
+    public override void Apply(IClientBuilder builder)
+    {
+        builder.Services.AddTransient<RabbitMQQueueAdapterFactory>();
+        builder.AddStreaming().AddPersistentStreams(StreamProviders.Default, AdapterFactory, ConfigureStream);
+        builder.Configure<ClusterOptions>(x =>
+        {
+            x.ClusterId = _clusterId;
+            x.ServiceId = _serviceId;
+        });
+        builder.UseMongoDBClient(_connectionString);
+        builder.UseMongoDBClustering(options =>
+        {
+            options.DatabaseName = _clusterId;
+            options.ClientName = _serviceId;
+            options.Strategy = MongoDBMembershipStrategy.SingleDocument;
+        });
+        base.Apply(builder);
+    }
+
+
+    private void ConfigureStream(IClusterClientPersistentStreamConfigurator obj)
+    {
+        // empty
+    }
+
+    private IQueueAdapterFactory AdapterFactory(IServiceProvider sp, string name)
+    {
+        return sp.GetRequiredService<RabbitMQQueueAdapterFactory>();
+    }
+}
---
+++ src/Modules/Infrastructure/Configuration/SiloStrategies/KubernetesSiloConfigurationStrategy.cs
@@ -0,0 +1,80 @@
+﻿using Core;
+using Orleans.Configuration;
+using Orleans.Providers.MongoDB.Configuration;
+using Orleans.Streams;
+using Serilog;
+using Streams;
+
+namespace Infrastructure.Configuration.SiloStrategies;
+
+public class KubernetesSiloConfigurationStrategy : SiloConfigurationStrategyBase
+{
+    private readonly string _clusterId;
+    private readonly string _connectionString;
+    private readonly int _gatewayPort;
+    private readonly string _serviceId;
+    private readonly int _siloPort;
+
+    public KubernetesSiloConfigurationStrategy(int siloPort, string clusterId, string serviceId,
+        string? connectionString,
+        int gatewayPort, SiloType siloType, string? discordToken = "") :
+        base(siloType)
+    {
+        _siloPort = siloPort;
+        _clusterId = clusterId;
+        _serviceId = serviceId;
+        ArgumentException.ThrowIfNullOrEmpty(connectionString, nameof(connectionString));
+        _connectionString = connectionString;
+        _gatewayPort = gatewayPort;
+    }
+
+    public override void Apply(ISiloBuilder builder, IConfiguration configuration)
+    {
+        builder.ConfigureLogging(loggingBuilder => { loggingBuilder.AddSerilog(); });
+        builder.Services.AddOptions().Configure<RabbitMQSettings>(configuration.GetSection("RabbitMQSettings"));
+        builder.Services.AddTransient<RabbitMQQueueAdapterFactory>();
+        builder.AddStreaming().AddPersistentStreams(StreamProviders.Default, AdapterFactory, ConfigureStream);
+        builder.ConfigureEndpoints(_siloPort, _gatewayPort, listenOnAnyHostAddress: true);
+        builder.Configure<ClusterOptions>(x =>
+        {
+            x.ClusterId = _clusterId;
+            x.ServiceId = _serviceId;
+        });
+        builder.UseMongoDBClient(_connectionString);
+        builder.UseMongoDBClustering(options =>
+        {
+            options.Strategy = MongoDBMembershipStrategy.SingleDocument;
+            options.ClientName = _serviceId;
+            options.DatabaseName = _clusterId;
+        });
+
+        builder.AddMongoDBGrainStorageAsDefault(options =>
+        {
+            options.DatabaseName = _clusterId;
+            options.ClientName = _serviceId;
+        });
+
+        builder.AddMongoDBGrainStorage("PubSubStore", options =>
+        {
+            options.ClientName = _serviceId;
+            options.DatabaseName = _clusterId;
+        });
+        builder.UseMongoDBReminders(options =>
+        {
+            options.ClientName = _serviceId;
+            options.DatabaseName = _clusterId;
+        });
+        base.Apply(builder, configuration);
+    }
+
+
+    private void ConfigureStream(ISiloPersistentStreamConfigurator configurator)
+    {
+        // empty
+    }
+
+    private IQueueAdapterFactory AdapterFactory(IServiceProvider sp, string name)
+    {
+        return sp.GetRequiredService<RabbitMQQueueAdapterFactory>();
+    }
+}
